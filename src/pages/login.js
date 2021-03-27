53b50620578ea5781141fcc4015f7757238939bf
Sean Adamson <seanadamson@Seans-MBP.broadband>
2021-03-27 12:37:15 +0000

    feat: login page complete

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext } from "react";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
+import * as ROUTES from "../constants/routes";
 
 function Login() {
   const history = useHistory();
@@ -12,13 +13,72 @@ function Login() {
   const [error, setError] = useState("");
   const isInvalid = password === "" || email === "";
 
-  function handleLogin() {}
+  async function handleLogin(event) {
+    event.preventDefault();
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      history.push(ROUTES.DASHBOARD);
+    } catch (error) {
+      setEmail("");
+      setPassword("");
+      setError(error.message);
+    }
+  }
 
   useEffect(() => {
     document.title = "Login - Instagram";
   }, []);
 
-  return <div>LOGIN</div>;
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="iphone with profile" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <div className="flex flex-col items-center bg-white p-4 border rounded border-gray-primary mb-4">
+          <h1 className="flex justify-center w-full">
+            <img src="/images/logo.png" className="mt-2 w-6/12 mb-4" />
+          </h1>
+
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              value={email}
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <input
+              aria-label="Enter your password"
+              value={password}
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${
+                isInvalid && "opacity-50"
+              }`}
+            >
+              Log in
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex w-full bg-white p-4 border rounded border-gray-primary">
+          <span className="text-sm">Don't have an account? </span>
+          <Link to="/signup" className="text-sm text-blue-medium ml-1">
+            Signup
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,32 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fill: (theme) => ({
+        red: theme("colors.red.primary"),
+      }),
+      colors: {
+        white: "#ffffff",
+        blue: {
+          medium: "#005c98",
+        },
+        black: {
+          light: "#005c98",
+          faded: "#00000059",
+        },
+        gray: {
+          base: "#616161",
+          background: "#fafafa",
+          primary: "#dbdbdb",
+        },
+        red: {
+          primary: "#ed4956",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
