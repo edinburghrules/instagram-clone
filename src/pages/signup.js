7dac6f5a00623aee56eb97d0a1370f9b5a73d18b
Sean Adamson <seanjadamson@gmail.com>
2021-03-27 13:00:50 +0000

    feat: start sign up page

---
+++ src/pages/signup.js
@@ -0,0 +1,94 @@
+import { useState, useEffect, useContext } from "react";
+import { Link, useHistory } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
+import * as ROUTES from "../constants/routes";
+
+function Signup() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [username, setUsername] = useState("");
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const isInvalid = password === "" || email === "";
+
+  async function handleSignup(event) {
+    event.preventDefault();
+  }
+
+  useEffect(() => {
+    document.title = "Login - Instagram";
+  }, []);
+
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="iphone with profile" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <div className="flex flex-col items-center bg-white p-4 border rounded border-gray-primary mb-4">
+          <h1 className="flex justify-center w-full">
+            <img src="/images/logo.png" className="mt-2 w-6/12 mb-4" />
+          </h1>
+
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+          <form onSubmit={handleSignup} method="POST">
+            <input
+              aria-label="Enter your username"
+              value={username}
+              type="text"
+              placeholder="Username"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+            <input
+              aria-label="Enter your full name"
+              value={fullname}
+              type="text"
+              placeholder="Full name"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setFullname(target.value)}
+            />
+            <input
+              aria-label="Enter your email address"
+              value={email}
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <input
+              aria-label="Enter your password"
+              value={password}
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${
+                isInvalid && "opacity-50"
+              }`}
+            >
+              Sign up
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex w-full bg-white p-4 border rounded border-gray-primary">
+          <span className="text-sm">Have an account? </span>
+          <Link to="/login" className="text-sm text-blue-medium ml-1">
+            Login
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
