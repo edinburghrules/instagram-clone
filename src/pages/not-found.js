c3dec11b19e33a035e2deee9a60cb556a4a165cb
Sean Adamson <seanjadamson@gmail.com>
2021-04-05 11:10:14 +0100

    feature: header && auth listener hook

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export default function NotFound() {
   useEffect(() => {
     document.title = "Not Found - Instagram";
-  });
+  }, []);
   return (
     <div className="bg-gray-background">
       <div className="mx-auto max-w-screen-lg">
